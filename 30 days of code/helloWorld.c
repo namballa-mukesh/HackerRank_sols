6174990a35ca21b07a5bd292909a35c164ccb91e
mukesh <44917136+namballa-mukesh@users.noreply.github.com>
2020-03-19 00:07:11 +0530

    Update helloWorld.c

--- 30 days of code/helloWorld.c
+++ 30 days of code/helloWorld.c
@@ -1,4 +1,8 @@
-
+/*
+Task :
+To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, 
+and finally print the value of your variable on a second line.
+*/
 int main() {
 
     char input_string[105]; 
