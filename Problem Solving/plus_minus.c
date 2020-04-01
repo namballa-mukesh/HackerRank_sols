de55439017e3d1ad318fd0d5e222a9b3f15f241d
mukesh <44917136+namballa-mukesh@users.noreply.github.com>
2020-04-01 09:35:46 +0530

    Create plus_minus.c

---
+++ Problem Solving/plus_minus.c
@@ -0,0 +1,34 @@
+// Problem Link : 
+
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    float x=0,y=0,z=0;
+    for(i=0;i<n;i++) {
+        scanf("%d",&a[i]);
+    if(a[i]>0) {
+        x++;
+    }
+    else if(a[i]<0) {
+        y++;
+    }
+    else {
+        z++;
+    }
+    }
+    printf("%6f\n", x/n);
+    printf("%6f\n", y/n);
+    printf("%6f\n", z/n);
+
+}
