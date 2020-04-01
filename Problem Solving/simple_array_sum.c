f88d0e26aa1e390d074b48d2f66a069fde4e57a9
mukesh <44917136+namballa-mukesh@users.noreply.github.com>
2020-04-01 09:24:52 +0530

    Create simple_array_sum.c

---
+++ Problem Solving/simple_array_sum.c
@@ -0,0 +1,14 @@
+// Problem Link : https://www.hackerrank.com/challenges/simple-array-sum/problem
+
+#include<stdio.h>
+int main(){
+    int n,i,sum=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
+    }
+    printf("%d",sum);
+}
